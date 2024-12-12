9bb26a116785c63b2c8b9432d774db284f91a682
Sahaj Singhal <sahajsinghal30@gmail.com>
2024-12-12 14:15:07 +0530

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -2,58 +2,44 @@ const squareRoot = function (number) {
   return Math.sqrt(number);
 }
 
-const selectOdds = function (number) {
-  if (number & 1) {
-    return number;
-  }
+const isOdd = function (number) {
+    return number % 2 !== 0;
 }
 
 const half = function (number) {
   return number / 2;
 }
 
-const lengthAbove = function (string) {
-  if (string.length > 5) {
-    return string;
-  }
+const isLengthAbove = function (string) {
+    return string.length > 5;
 }
 
 const capitalise = function (string) {
   return string.toUpperCase();
 }
 
-const selectFromArray = function (array) {
+const filter = function (array, selectFrom) {
   const selectives = [];
 
   for (const element in array) {
-    if (array[element] !== undefined) {
-      selectives.push(array[element]);
+    if (array[element] === true) {
+      selectives.push(selectFrom[element]);
     }
   }
 
   return selectives;
 }
 
-const fnCall = function (fnName, array) {
+const mapTo = function (fnName, array) {
   const result = [];
 
   for (const element in array) {
     result.push(fnName(array[element]));
   }
 
-  if (fnName === selectOdds || fnName === lengthAbove) {
-    return selectFromArray(result);
+  if (fnName === isOdd || fnName === isLengthAbove) {
+    return filter(result, array);
   }
 
   return result;
 }
-
-const test = function () {
-  console.log(fnCall(squareRoot, [1, 2, 4, 9]));
-  console.log(fnCall(selectOdds, [1, 2, 4, 9]));
-  console.log(fnCall(half, [1, 2, 4, 9]));
-  console.log(fnCall(lengthAbove, ['a', 'aaaaa', 'aaaaaa', 'aaaaaaa']));
-  console.log(fnCall(capitalise, ['a', 'aaaaa', 'aaaaaa', 'aaaaaaa']));
-}
-
-test();
